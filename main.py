dfdd5eca26bb1b3d9c1990e17ec3a4973a4daffe
Your <you@example.com>
2025-07-29 18:05:29 +0300

    "parser"

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from Lexer import Lexer
+from Parser import Parser
+from AST import Program
+import json
 
-LEXER_DEBUG : bool = True
+
+LEXER_DEBUG : bool = False
+PARSER_DEBUG : bool = True
+
 
 if __name__ == '__main__':
-    with open ("tests/lexer.th" , "r") as f:
+    with open ("tests/parser.th" , "r") as f:
         code: str =f.read()
 
     if LEXER_DEBUG:
@@ -11,3 +17,15 @@ if __name__ == '__main__':
 
         while debug_lex.current_char is not None:
             print(debug_lex.next_token())    
+
+    l : Lexer = Lexer(source=code)
+    p : Parser = Parser(lexer=l)
+
+    if PARSER_DEBUG:
+        print("===== Parser Debug =====")
+        program : Program = p.parse_program()
+
+        with open("debug/ast.json" , "w" ) as f:
+            json.dump(program.json() , f, indent=4)
+
+        print("Ast json file created successfully")
